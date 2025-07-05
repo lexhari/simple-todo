33a38bbf6141a50d7e18351f98b21fd68d6640eb
Harriet Cardenas <lolitsmealexis@gmail.com>
2025-07-05 12:29:35 +0800

    build: add tailwind css configuration
    Add tailwind.config.js with NativeWind preset for React Native styling support

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+const index = () => {
+  return (
+    <View>
+        <Text>hello</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
